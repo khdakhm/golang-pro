6d096c7b315350afe31dd0440fcb3bae34e35244
khdakhm <khedaakhmathanova@gmail.com>
2025-12-03 15:05:38 +0300

    checking again

--- main.go
+++ main.go
@@ -6,10 +6,6 @@ import (
 )
 
 func main() {
-<<<<<<< HEAD
 	fmt.Println("проверка!")
-=======
-	fmt.Println("checking again")
->>>>>>> b4dddcf180f16e2a3939405389f481e3a6730bd6
 	fmt.Println("Сейчас время:", time.Now()) // Теперь time не подсвечивается красным
 }
